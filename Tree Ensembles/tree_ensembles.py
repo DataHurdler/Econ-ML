5733dc2e6fe74d4da32313a7844e047b98f9b768
Zijun Luo <zijunluo8620@gmail.com>
2023-05-26 13:43:10 -0500

    Update tree_ensembles.py

--- Tree Ensembles/tree_ensembles.py
+++ Tree Ensembles/tree_ensembles.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import GradientBoostingClassifier
@@ -17,6 +18,10 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 import xgboost as xgb
 
+N_GROUP = 5
+N_IND = 10000
+N_FEATURES = 10
+
 
 class TreeModels:
     def __init__(
@@ -25,7 +30,7 @@ class TreeModels:
             n_individuals: int = 10000,
             n_num_features: int = 5,
     ):
-        print(f'There are {n_individuals} people.')
+        print(f'There are {n_individuals} individuals.')
         print(f'There are {n_group} choices.')
         print(f'There are {n_num_features} numerical features and 1 categorical feature.')
 
@@ -52,20 +57,28 @@ class TreeModels:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             self.X, self.y, test_size=0.3, random_state=42)
 
-    def AllResults(
+        self.y_pred = np.empty([n_individuals, 1])
+
+    def show_results(
             self,
             clf,
     ):
         clf.fit(self.X_train, self.y_train)
         self.y_pred = clf.predict(self.X_test)
-        print(f'Feature Importance: {clf.feature_importances_}')
+
+        if clf == logit:
+            print(f'Coefficients: {clf.coef_}')
+        else:
+            print(f'Feature Importance: {clf.feature_importances_}')
 
         train_acc = clf.score(self.X_train, self.y_train)
         print(f'Training accuracy: {train_acc:.4f}')
         acc = accuracy_score(self.y_test, self.y_pred)
         print(f'Test accuracy: {acc:.4f}')
         precision = precision_score(self.y_test, self.y_pred, average='weighted')
-        print(f'Test precisions: {precision:.4f}')
+        print(f'Test precision: {precision:.4f}')
+        recall = recall_score(self.y_test, self.y_pred, average='weighted')
+        print(f'Test recall: {recall:.4f}')
         f1 = f1_score(self.y_test, self.y_pred, average='weighted')
         print(f'Test F1 score: {f1:.4f}')
 
@@ -80,19 +93,25 @@ class TreeModels:
 
 
 if __name__ == "__main__":
-    tree = TreeModels(n_num_features=10)
+    # No interruption by plt.show()
+    plt.ion()
+
+    tree = TreeModels(n_num_features=N_FEATURES)
+
+    logit = LogisticRegression(max_iter=10000)
+    tree.show_results(logit)
 
     d_tree = DecisionTreeClassifier()
-    tree.AllResults(d_tree)
+    tree.show_results(d_tree)
 
     rf = RandomForestClassifier()
-    tree.AllResults(rf)
+    tree.show_results(rf)
 
     ada = AdaBoostClassifier()
-    tree.AllResults(ada)
+    tree.show_results(ada)
 
     gbm = GradientBoostingClassifier()
-    tree.AllResults(gbm)
+    tree.show_results(gbm)
 
     xgb = xgb.XGBClassifier()
-    tree.AllResults(xgb)
+    tree.show_results(xgb)
