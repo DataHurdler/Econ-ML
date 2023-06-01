3d4f6821bf755cbce21b8c8145355c366d4360e7
Zijun Luo <zijunluo8620@gmail.com>
2023-05-31 21:49:28 -0500

    added docstring by GPT

--- Multi-Arm Bandits/bayesianab_mc.py
+++ Multi-Arm Bandits/bayesianab_mc.py
@@ -17,6 +17,21 @@ M = 2000
 
 
 def worker(algo, number_of_bandits, number_of_trials, p_max, p_diff, p_min, n):
+    """
+        Worker function to run the BayesianAB algorithm for a single trial.
+
+        Args:
+            algo (str): Name of the algorithm.
+            number_of_bandits (int): Number of bandits.
+            number_of_trials (int): Number of trials.
+            p_max (float): Maximum probability of winning.
+            p_diff (float): Difference in probabilities of winning.
+            p_min (float): Minimum probability of winning.
+            n (int): Index of the trial.
+
+        Returns:
+            list: History of bandit wins for the given trial.
+    """
     bayesianab_instance = BayesianAB(number_of_bandits, number_of_trials, p_max, p_diff, p_min)
     getattr(bayesianab_instance, algo)()
     return bayesianab_instance.history_bandit
@@ -30,6 +45,20 @@ def monte_carlo(
         p_diff: float = .05,
         p_min: float = .1
 ):
+    """
+    Runs multiple trials of different algorithms using Monte Carlo simulation.
+
+    Args:
+        algos (list): List of algorithm names to compare.
+        m (int): Number of trials. Default is 500.
+        n (int): Number of visitors. Default is 10001.
+        p_max (float): Maximum probability of winning. Default is 0.75.
+        p_diff (float): Difference in probabilities of winning. Default is 0.05.
+        p_min (float): Minimum probability of winning. Default is 0.1.
+
+    Returns:
+        dict: Dictionary containing the history of bandit wins for each algorithm.
+    """
     algos_hist = {algo: [] for algo in algos}
 
     for algo in algos:
@@ -49,6 +78,18 @@ def run_monte_carlo(
         n,
         p_values,
 ):
+    """
+    Runs the Monte Carlo simulation for different probability configurations.
+
+    Args:
+        algos (list): List of algorithm names to compare.
+        m (int): Number of trials.
+        n (int): Number of visitors.
+        p_values (list): List of probability configurations.
+
+    Returns:
+        dict: Dictionary containing the DataFrame of bandit wins for each probability configuration.
+    """
     trials = {}
     df_all = {}
 
@@ -59,13 +100,14 @@ def run_monte_carlo(
                                       n,
                                       p_values[i][0],
                                       p_values[i][1],
-                                      p_values[i][2],)
+                                      p_values[i][2], )
 
     for i in range(len(p_values)):
         df = pd.DataFrame()
         for j in algos:
             lst = [0] * (N - 1)
             for k in range(M):
+                # Calculate the cumulative bandit wins for each trial and algorithm
                 lst = np.array(lst) + np.array([1 if x == 4 else 0 for x in trials[f'p{i}'][j][k]])
             df[j] = (lst / M).tolist()
 
@@ -80,6 +122,18 @@ def plot_monte_carlo(
         col,
         row,
 ):
+    """
+    Plots the results of the Monte Carlo simulation.
+
+    Args:
+        df_all (dict): Dictionary containing the DataFrame of bandit wins for each probability configuration.
+        algos (list): List of algorithm names to compare.
+        col (int): Number of columns in the plot.
+        row (int): Number of rows in the plot.
+
+    Returns:
+        None
+    """
     figure, axis = plt.subplots(row, col, figsize=(20, 10))
     colors = sns.color_palette("Set2", len(algos))
 
@@ -89,6 +143,7 @@ def plot_monte_carlo(
     for key in df_all:
         ax = axis[n, m]
         for i in range(len(algos)):
+            # Plot the line graph for each algorithm
             sns.lineplot(x=df_all[key].index, y=df_all[key][algos[i]], linewidth=0.5, color=colors[i], ax=ax)
 
         ax.set_ylabel('')
