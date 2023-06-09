b195acce1e0d9009866f53c0ca1eb9ad5d684ca4
Zijun Luo <zijunluo8620@gmail.com>
2023-06-08 22:42:21 -0500

    Update tree_ensembles_mc.py

--- Tree Ensembles/tree_ensembles_mc.py
+++ Tree Ensembles/tree_ensembles_mc.py
@@ -5,9 +5,9 @@ from multiprocessing import Pool, cpu_count
 from functools import partial
 from tree_ensembles import run_tree_ensembles
 
-# plt.ion()
+plt.ion()
 
-n_individuals_range = range(1000, 3001, 1000)
+n_individuals_range = range(5000, 50001, 5000)
 
 
 def run_monte_carlo(n_individuals_range, numeric_only_bool):
@@ -27,7 +27,10 @@ def plot_monte_carlo(data: list):
             for j, inner_inner_dict in inner_dict.items():
                 value = inner_inner_dict['cv_score']
                 df_list.append({'i': i, 'Model': j, 'cv_score': value})
+
     df = pd.DataFrame(df_list)
+
+    fig, ax = plt.subplots()
 
     num_models = len(df['Model'].unique())
     cmap = plt.get_cmap('Set2')  # Use the Set2 color map
@@ -35,15 +38,15 @@ def plot_monte_carlo(data: list):
     for i, model in enumerate(df['Model'].unique()):
         model_data = df[df['Model'] == model]
         color = cmap(i % num_models)  # Cycle through the color map
-        plt.scatter(model_data['i'], model_data['cv_score'], c=color, label=model, alpha=0.5, s=50)
+        ax.scatter(model_data['i'], model_data['cv_score'], c=color, label=model, alpha=0.5, s=50)
 
-    plt.xlabel('Number of Individuals')
-    plt.ylabel('Cross Validation Scores')
-    plt.title('Plot of Cross Validation Scores')
-    plt.legend(['Logit', 'Decision Tree', 'Random Forest', 'Adaboost', 'GBM', 'XGBoost'],
-               loc='lower right',
-               fontsize=9, markerscale=1.5, scatterpoints=1,
-               fancybox=True, framealpha=0.5)
+    ax.set_xlabel('Number of Individuals')
+    ax.set_ylabel('Cross Validation Scores')
+    ax.set_title('Plot of Cross Validation Scores')
+    ax.legend(['Logit', 'Decision Tree', 'Random Forest', 'Adaboost', 'GBM', 'XGBoost'],
+              loc='lower right',
+              fontsize=9, markerscale=1.5, scatterpoints=1,
+              fancybox=True, framealpha=0.5)
 
     # plt.show()
 
@@ -53,6 +56,6 @@ if __name__ == "__main__":
     plot_monte_carlo(mc_output)
     plt.savefig(f"comparison_false.png", dpi=300)
 
-    mc_output = run_monte_carlo(n_individuals_range, [True])
-    plot_monte_carlo(mc_output)
+    mc_output_true = run_monte_carlo(n_individuals_range, [True])
+    plot_monte_carlo(mc_output_true)
     plt.savefig(f"comparison_true.png", dpi=300)
