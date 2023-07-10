e895ecc2942977b54f1c7d8bb44f749644553f1d
Zijun Luo <zijunluo8620@gmail.com>
2023-07-10 14:43:42 -0500

    father comparison into a single script

--- Forecasting/ts_comparison.py
+++ Forecasting/ts_comparison.py
@@ -58,3 +58,29 @@ if __name__ == "__main__":
 
     print(results.head(10))
     results.to_csv('results.csv', index=False)
+
+    df = pd.read_csv("results.csv")
+
+    # Calculate the average value for each model and type
+    averages = df.groupby(['model', 'n_test'])['mape'].mean().reset_index()
+
+    fig, ax = plt.subplots(figsize=(15, 5))
+    # Create a line plot for each model
+    models = averages['model'].unique()
+    for model in models:
+        # Determine the line style based on the model name
+        linestyle = '--o' if 'single' in model else '-o'
+
+        model_data = averages[averages['model'] == model]
+        ax.plot(model_data['n_test'], model_data['mape'], linestyle, label=model)
+
+    # Set plot title and labels
+    ax.set_title('Average Value by Type for Each Model')
+    ax.set_xlabel('Forecast Horizon')
+    ax.set_ylabel('Mean Absolute Percentage Error')
+
+    # Show a legend indicating the models
+    plt.legend()
+
+    # Save the plot
+    plt.savefig("comparison_dl.png", dpi=300)
