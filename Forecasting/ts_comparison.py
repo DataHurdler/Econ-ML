f2e82c92b9e207d4326716b07af12155b629c7bf
Zijun Luo <zijunluo8620@gmail.com>
2023-07-10 15:36:37 -0500

    Update ts_comparison.py

--- Forecasting/ts_comparison.py
+++ Forecasting/ts_comparison.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import random
 
 from sklearn.metrics import mean_absolute_percentage_error
 
@@ -101,77 +104,21 @@ def plot_comparison(df, load_df=False):
 if __name__ == "__main__":
     plt.ion()
 
+    # make sure to get reproducible results
+    np.random.seed(42)
+    random.seed(42)
+    tf.random.set_seed(42)
+
     COL = 'Log'
-    # N_TEST_list = (5, 10)
-    # stock_list = ('AAPL', 'UAL')
-    STOCK_LIST = ('AAPL', 'UAL', 'WMT', 'PFE', 'MA', 'MCD', 'OXY', 'BA', 'GE', 'GM')
-    N_TEST_LIST = (5, 10, 20, 50, 100, 200)
     DL_MODELS = {
         "ann": ('ann', None),
         "cnn": ('cnn', None),
         "lstm": ('rnn', 'lstm'),
         "gru": ('rnn', 'gru'),
     }
+    STOCK_LIST = ('AAPL', 'UAL', 'WMT', 'PFE', 'MA', 'MCD', 'OXY', 'BA', 'GE', 'GM')
+    N_TEST_LIST = (5, 10, 20, 50, 100, 200)
     EPOCHS = 1500
 
     comp_results = run_comparison(COL, STOCK_LIST, N_TEST_LIST, DL_MODELS, EPOCHS)
     plot_comparison(comp_results)
-
-    # column_names = ['model', 'stock', 'n_test', 'mape']
-    # results = pd.DataFrame(columns=column_names)
-    #
-    # for n in N_TEST_list:
-    #     for stock in stock_list:
-    #         p = StocksForecastProphet(stock_name_list=stock_list, n_test=n)
-    #
-    #         p.run_prophet(col=col, stock_name=stock)
-    #         mape = mean_absolute_percentage_error(p.dfs[stock]['yhat'][-n:], p.dfs[stock]['y'][-n:])
-    #         new_row = ['prophet', stock, n, mape]
-    #         results.loc[len(results)] = new_row
-    #         for model in DL_models:
-    #             dl = StocksForecastDL(stock_name_list=stock_list, n_test=n, epochs=50)
-    #             if DL_models[model][0] == 'rnn':
-    #                 dl.single_model_comparison(col=[col],
-    #                                            stock_name=stock,
-    #                                            model=DL_models[model][0],
-    #                                            rnn_model=DL_models[model][1])
-    #             else:
-    #                 dl.single_model_comparison(col=[col],
-    #                                            stock_name=stock,
-    #                                            model=DL_models[model][0])
-    #
-    #             mape1 = mean_absolute_percentage_error(dl.dfs[stock]['multistep'][-n:], dl.dfs[stock][col][-n:])
-    #             mape2 = mean_absolute_percentage_error(dl.dfs[stock]['multioutput'][-n:], dl.dfs[stock][col][-n:])
-    #             new_row1 = [f"{model}_single", stock, n, mape1]
-    #             new_row2 = [f"{model}_multi", stock, n, mape2]
-    #             results.loc[len(results)] = new_row1
-    #             results.loc[len(results)] = new_row2
-    #
-    # print(results.head(10))
-    # results.to_csv('results.csv', index=False)
-
-    # df = pd.read_csv("results.csv")
-    #
-    # # Calculate the average mape for each model and type
-    # averages = df.groupby(['model', 'n_test'])['mape'].mean().reset_index()
-    #
-    # # Create a line plot for each model
-    # fig, ax = plt.subplots(figsize=(15, 5))
-    # models = averages['model'].unique()
-    # for model in models:
-    #     # Determine the line style based on the model name
-    #     linestyle = '--o' if 'single' in model else '-o'
-    #
-    #     model_data = averages[averages['model'] == model]
-    #     ax.plot(model_data['n_test'], model_data['mape'], linestyle, label=model)
-    #
-    # # Set plot title and labels
-    # ax.set_title('Average Value by Type for Each Model')
-    # ax.set_xlabel('Forecast Horizon')
-    # ax.set_ylabel('Mean Absolute Percentage Error')
-    #
-    # # Show a legend indicating the models
-    # plt.legend()
-    #
-    # # Save the plot
-    # plt.savefig("comparison_dl.png", dpi=300)
